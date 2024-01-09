a42bf5d35345674ab75acdfcc85bc2422ce2b076
Chadow7 <97482495+Chadow7@users.noreply.github.com>
2024-01-09 01:56:19 +0100

    commit

--- react/src/pages/Home.jsx
+++ react/src/pages/Home.jsx
@@ -4,8 +4,34 @@ import { Link } from 'react-router-dom';
 export default function Home() {
   return (
     <div>
-      <h2>Home</h2>
-      <Link to="/articles">Go to Articles</Link>
+      <h2>Accueil</h2>
+      <p> 
+        Yute est bien plus qu'un simple guide pour votre première année en BUT Informatique à l'IUT de Vannes. Nous sommes votre compagnon tout au long de votre parcours universitaire, offrant un soutien continu pour chaque étape, de la première année jusqu'à l'obtention de votre diplôme.
+      </p>
+      <h3>Pourquoi Yute reste votre allié ?</h3>
+      
+      <h4>Accès aux Professeurs et à Leur Description :</h4>
+      <p> 
+        Consultez facilement la liste des professeurs avec leurs descriptions détaillées. Comprenez leurs domaines d'expertise et pourquoi vous pourriez avoir besoin de les rencontrer.
+
+      </p>
+      <h4>Utilité de Rencontrer les Professeurs :</h4>
+      <p> 
+        Découvrez pourquoi il est crucial de rencontrer vos professeurs, que ce soit pour des conseils académiques, des opportunités de recherche ou même des orientations pour votre future carrière.
+      </p>
+
+      <h4>Utilisation de la Carte Étudiante :</h4>
+      <p> 
+        Apprenez comment tirer le meilleur parti de votre carte étudiante, des services offerts sur le campus aux réductions disponibles à l'extérieur de l'université.
+      </p>
+      <h4>Informations Pratiques au-delà de la Première Année :</h4> 
+      <p>
+        Explorez une multitude d'informations pratiques sur la vie étudiante, des conseils pour gérer les études avancées à des ressources pour des stages, des programmes d'échange ou des projets de recherche.
+      </p>
+
+      <b> Bienvenue à bord de Yute, votre guide fiable pour un parcours universitaire épanouissant et réussi.</b>
+
     </div>
+  
   );
 };
