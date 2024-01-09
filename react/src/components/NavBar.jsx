6bdcb03bb8242845557a4e58c8c3604a432b37b2
Tonaxis <83670267+Tonaxis@users.noreply.github.com>
2024-01-09 01:11:03 +0100

    logo

--- react/src/components/NavBar.jsx
+++ react/src/components/NavBar.jsx
@@ -5,7 +5,7 @@ import Link from './Link';
 export default function NavBar() {
   return (
     <div className="nav-bar">
-        <img src="squirrel.webp" alt="logo" width={60} height={60} />
+        <img src="logo.svg" alt="logo" width={60} height={60} />
         <nav className="nav-bar__links">
           <Link className="nav-bar__links__link" to="/"><span className="nav-bar__links__link--text">Accueil</span> <Home className="nav-bar__links__link--icon" size={48} /></Link>
           <Link className="nav-bar__links__link" to="/about"><span className="nav-bar__links__link--text">A Propos</span> <Info className="nav-bar__links__link--icon" size={48} /></Link>
