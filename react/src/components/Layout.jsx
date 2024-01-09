5598d839ed3f8c2f3faf58375815a56c32aadc4e
Chadow7 <97482495+Chadow7@users.noreply.github.com>
2024-01-09 02:18:31 +0100

    nv pages prof

--- react/src/components/Layout.jsx
+++ react/src/components/Layout.jsx
@@ -11,9 +11,9 @@ export default function Layout({ children }) {
       <NavBar />
       <div className="layout">
           <div className={`layout__part layout__part--left-sidebar ${isSidebarOpen ? 'active' : ''}`} >
-              <h3>Menu</h3>
+              <h3>Pages</h3>
               <nav className="layout__part--left-sidebar__links">
-                <Link className="layout__part--left-sidebar__links__link" to="/test">Test</Link>
+                <Link className="layout__part--left-sidebar__links__link" to="/test"> </Link>
                 <Link className="layout__part--left-sidebar__links__link" to="/test">Testssss</Link>
               </nav>
               <button className="layout__part layout__part--left-sidebar__button" onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
