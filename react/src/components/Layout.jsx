f456bacd75e7a1fe21342550d54aff695cc96eba
Chadow7 <97482495+Chadow7@users.noreply.github.com>
2024-01-09 02:24:08 +0100

    kk

--- react/src/components/Layout.jsx
+++ react/src/components/Layout.jsx
@@ -13,8 +13,16 @@ export default function Layout({ children }) {
           <div className={`layout__part layout__part--left-sidebar ${isSidebarOpen ? 'active' : ''}`} >
               <h3>Pages</h3>
               <nav className="layout__part--left-sidebar__links">
-                <Link className="layout__part--left-sidebar__links__link" to="/test"> </Link>
-                <Link className="layout__part--left-sidebar__links__link" to="/test">Testssss</Link>
+                <Link className="layout__part--left-sidebar__links__link" to="/tuffigo"> Hélène Tuffigo</Link>
+                <Link className="layout__part--left-sidebar__links__link" to="/merciol">François Merciol</Link>
+                <Link className="layout__part--left-sidebar__links__link" to="/kamp">Jean François Kamp</Link>
+                <Link className="layout__part--left-sidebar__links__link" to="/pham">Minh Tan Pham</Link>
+                <Link className="layout__part--left-sidebar__links__link" to="/godin">Thibault Godin</Link>
+                <Link className="layout__part--left-sidebar__links__link" to="/kerbellec">Goulven Kerbellec</Link>
+                <Link className="layout__part--left-sidebar__links__link" to="/roirand">Xavier Roirand</Link>
+                <Link className="layout__part--left-sidebar__links__link" to="/tonin">Philipe Tonin</Link>
+                <Link className="layout__part--left-sidebar__links__link" to="/ferragut">Tom Ferragut</Link>
+                <Link className="layout__part--left-sidebar__links__link" to="/belmouhcine">Abdelbadi Belmouhcine</Link>
               </nav>
               <button className="layout__part layout__part--left-sidebar__button" onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
                 <span><ChevronLeft /></span>
